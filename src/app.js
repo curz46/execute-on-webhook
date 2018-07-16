f3d19e8b2a32ec65883ee1df3a13c9a4da307f54
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2018-07-16 20:51:59 +0100

    make sync + add detached param

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import "babel-polyfill";
 
 import util from 'util';
 import child_process from 'child_process';
-const exec = util.promisify(child_process.exec);
 
 import { config as dotenv } from 'dotenv';
 dotenv();
@@ -17,11 +16,11 @@ app.get('/push/:key', async (req, res) => {
     if (key === AUTH_KEY) {
         res.status(200).json({ result: 'success' });
         for (const cmd of COMMANDS) {
-            await exec(cmd);
+            child_process.execSync(cmd, { detached: true });
         }
     } else {
         res.status(500).json({ result: 'unauthorized' });
     }
 });
 
-app.listen(PORT, () => console.log('listening on *:3000'));
+app.listen(PORT, () => console.log('listening on *:', PORT));
