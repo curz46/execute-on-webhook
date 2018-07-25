aac21df7a3bb50ee4b9562f591c1c8c5ae445d02
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2018-07-25 20:08:29 +0100

    change to listen to POST requests; add branch rerequirements; make some config properties optional

--- config.example.js
+++ config.example.js
@@ -2,7 +2,9 @@ module.exports = {
     // The port to run the webserver on.
     port: 80,
     // Make this really long to make it impossible that an unauthorised request goes through.
-    key: "super secret key",
+    key: 'super secret key',
+    // Optional branch name. If set, commands will only fire if the name of the branch is contained in this array.
+    branches: [],
     // These will execute sequentially.
     commands: [
         'echo hello >> .somefile'
--- src/app.js
+++ src/app.js
@@ -1,26 +1,43 @@
 import "babel-polyfill";
 
-import util from 'util';
 import child_process from 'child_process';
 
 import { config as dotenv } from 'dotenv';
 dotenv();
 
 import express from "express";
+import bodyParser from 'body-parser';
 const app = express();
 
-import {key as AUTH_KEY, commands as COMMANDS, port as PORT} from '../config.js';
+app.use(bodyParser.json());
 
-app.get('/push/:key', async (req, res) => {
+import config from '../config.js';
+if (!config.port || !config.commands) {
+    console.log('config.js must contain at least port and commands.');
+    process.exit();
+}
+const { port: PORT, commands: COMMANDS } = config;
+const AUTH_KEY = config.key;
+const BRANCHES = config.branches;
+
+app.post('/push/:key', async (req, res) => {
     const {key} = req.params;
-    if (key === AUTH_KEY) {
-        res.status(200).json({ result: 'success' });
-        for (const cmd of COMMANDS) {
-            child_process.execSync(cmd, { detached: true });
+    console.log(key);
+    if (AUTH_KEY == null || key === AUTH_KEY) {
+        // body.ref e.g. refs/heads/master
+        const branch = req.body.ref ? req.body.ref.split('/').slice(-1)[0] : null;
+        if (BRANCHES != null && !BRANCHES.includes(branch)) {
+            // branches defined AND does not contain this branch, do nothing
+            res.status(200).json({ result: 'ignored-branch' });
+        } else {
+            for (const cmd of COMMANDS) {
+                child_process.execSync(cmd, { detached: true });
+            }
+            res.status(200).json({ result: 'success' });
         }
     } else {
-        res.status(500).json({ result: 'unauthorized' });
+        res.status(401).json({ result: 'unauthorized' });
     }
 });
 
-app.listen(PORT, () => console.log('listening on *:', PORT));
+app.listen(PORT, () => console.log('listening on *:' + PORT));
